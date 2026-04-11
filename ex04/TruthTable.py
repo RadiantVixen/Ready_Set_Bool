0ec2a775940fafeb08e5f2765131242f64f86e31
Aicha Atki <aatki@e2r8p7.1337.ma>
2026-04-11 12:03:35 +0100

    push

--- ex04/TruthTable.py
+++ ex04/TruthTable.py
@@ -1,10 +1,31 @@
-from ../ex03/BooleanEvaluation import eval_formula
+from ex03.BooleanEvaluation import eval_formula
 
 def  print_truth_table(s):
     if not s:
         return False
+    
+    n = 0
+    
+    map = {chr(i): 0 for i in range(ord('A'), ord('Z') + 1)}
+
+    for c in s:
+        if c.isalphabet():
+            n += 1
+
+    
+    s = list(s)
+    for i in range(len(s)):
+        if s[i].alphabet():
+            if map[s[i]] == 1:
+                s[i] = '1'
+            else:
+                s[i] = '0'
+    eval_formula(str(s))
 
 
 
 if __name__ == "__main__" :
-    
+     print("eval_formula:", eval_formula("AB&C|"))
+     print("print_truth_table:", print_truth_table("AB&C|"))
+
+
