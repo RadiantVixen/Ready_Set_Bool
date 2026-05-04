3c50f23197d5ccaf58d503511bbf4de61f674503
Aicha Atki <aatki@e2r8p7.1337.ma>
2026-05-04 23:05:50 +0100

    push

--- ex04/TruthTable.py
+++ ex04/TruthTable.py
@@ -1,31 +1,96 @@
-from ex03.BooleanEvaluation import eval_formula
+from collections import defaultdict
 
-def  print_truth_table(s):
+def  eval_formula(s)->bool:
     if not s:
         return False
+
+    stack = []
+    for c in s:
+        if c == '0' or c =='1':
+            stack.append(int(c))
+        elif c == '!':
+            if len(stack) < 1:
+                print("the formula is invalid")
+                return False
+            a = stack.pop()
+            stack.append(not a)
+        else:
+            if len(stack) < 2:
+                print("the formula is invalid")
+                return False
+            a = stack.pop()
+            b = stack.pop()
+            if c == '&':
+                stack.append(a & b)
+            elif c == '|':
+                stack.append(a | b)
+            elif c == '^':
+                stack.append(a ^ b)
+            elif c == '>':
+                stack.append(not (b == 0  and a == 1))
+            elif c == '=':
+                stack.append(a == b)
+            else:
+                print("the formula is invalid")
+                return False
     
+    if len(stack) > 1:
+        print("the formula is invalid")
+        return False
+    return bool(stack[0])
+
+
+
+def TruthTable(s):
+    mapping = defaultdict(int)
+    sl = list(s)
     n = 0
     
-    map = {chr(i): 0 for i in range(ord('A'), ord('Z') + 1)}
-
-    for c in s:
-        if c.isalphabet():
-            n += 1
-
-    
-    s = list(s)
     for i in range(len(s)):
-        if s[i].alphabet():
-            if map[s[i]] == 1:
-                s[i] = '1'
-            else:
-                s[i] = '0'
-    eval_formula(str(s))
+        if s[i].isalpha():
+            n +=1
+            mapping[s[i]] = 0
+
+    tab = list(mapping.keys())
+
+    print("| " + " | ".join(tab) + " | = |")
+    print("|" + "---|" * (len(tab) + 1))
+
+    def replace():
+        nonlocal s
+        nonlocal sl
+        nonlocal mapping
+
+        for i in range(len(s)):
+            if s[i].isalpha():
+                if mapping[s[i]] == 1:
+                    sl[i] = '1'
+                else:
+                    sl[i] = '0'
+
+
+    def rec(tab, i):
+        nonlocal mapping
+        nonlocal sl
+        
+        if len(tab) == i:
+            replace()
+            result = eval_formula("".join(sl))
+            print("| " + " | ".join(str(mapping[v]) for v in tab) + f" | {int(result)} |")
+            return
+        
+        mapping[tab[i]] = 0
+        rec(tab, i + 1)
+
+        mapping[tab[i]] = 1
+        rec(tab, i + 1)
+
+
+    rec(tab, 0)
 
 
 
-if __name__ == "__main__" :
-     print("eval_formula:", eval_formula("AB&C|"))
-     print("print_truth_table:", print_truth_table("AB&C|"))
 
+if __name__ == "__main__":
+    print(TruthTable("AB&C|"))
 
